9470f41a0d0c5f54dcb0c1fc6a38c1f08ea7ea59
SLW-Tc <stanislaus.lumen@gmail.com>
2026-07-24 07:47:33 +0700

    Add additional console log statements to hello.js

--- hello.js
+++ hello.js
@@ -1,1 +1,4 @@
 console.log("Hai, nama saya adalah XRPL5 yaitu Lumen ");
+console.log("11 April 2011");
+console.log("SMP Santa Lusia");
+console.log("Alasan memilih jurusan RPL karena saya ingin menjadi seorang programmer");
