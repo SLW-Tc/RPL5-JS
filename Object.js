e548244c3b263432d6315b7e902a2002724b8ea2
SLW-Tc <stanislaus.lumen@gmail.com>
2026-07-30 11:20:51 +0700

    Update Object.js

--- Object.js
+++ Object.js
@@ -5,6 +5,7 @@ const karakter = {
     senjata: "Pedang"
 };
 
+console.log(karakter);
 console.log("Name: " + karakter.nama + "\nlevel: " + karakter.level + "\nhp: " + karakter.hp + "\nsenjata: " + karakter.senjata);
 console.log(karakter ["hp"])
 console.log(karakter ["level"])
