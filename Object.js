c73fd83ac6b8df4f94b6a3e7f54de940ee3756ba
SLW-Tc <stanislaus.lumen@gmail.com>
2026-07-30 11:16:21 +0700

    Add files via upload

---
+++ Object.js
@@ -0,0 +1,10 @@
+const karakter = {
+    nama: "Lemon The Destroyer",
+    level: 100,
+    hp: 1000,
+    senjata: "Pedang"
+};
+
+console.log("Name: " + karakter.nama + "\nlevel: " + karakter.level + "\nhp: " + karakter.hp + "\nsenjata: " + karakter.senjata);
+console.log(karakter ["hp"])
+console.log(karakter ["level"])
