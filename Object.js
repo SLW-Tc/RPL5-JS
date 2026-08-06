ea5c03afd1e6b545f8016d3a102e945aa46da4b6
SLW-Tc <stanislaus.lumen@gmail.com>
2026-08-06 12:52:45 +0700

    Add files via upload

--- Object.js
+++ Object.js
@@ -1,11 +1,14 @@
 const karakter = {
     nama: "Lemon The Destroyer",
     level: 100,
-    hp: 1000,
+    hp: 2000,
     senjata: "Pedang"
 };
-
+//output
 console.log(karakter);
+//dot notation = karakter.nama, karakter.level, karakter.hp, karakter.senjata
+//template literal = "\nName: " + karakter.nama + "\nlevel: " + karakter.level + "\nhp: " + karakter.hp + "\nsenjata: " + karakter.senjata
 console.log("Name: " + karakter.nama + "\nlevel: " + karakter.level + "\nhp: " + karakter.hp + "\nsenjata: " + karakter.senjata);
+//bracket notation
 console.log(karakter ["hp"])
 console.log(karakter ["level"])
---
+++ karyawan.js
@@ -0,0 +1,11 @@
+//Membuat/deklarasi objek karyawan
+const karyawan = {
+    Nama : "Stanislaus Lumen",
+    Umur : 24,
+    Gaji : 10000000,
+    Karyawan : true,
+    Jabatan : null
+}
+
+//output dot notation + template literal informasi karyawan
+console.log("Nama: " + karyawan.Nama + "\nUmur: " + karyawan.Umur + "\nGaji: " + karyawan.Gaji + "\nKaryawan Tetap: " + karyawan.Karyawan + "\nJabatan: " + karyawan.Jabatan);
